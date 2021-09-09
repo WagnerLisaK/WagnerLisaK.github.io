30fc63e2b7989d13e5ae8444dceb284ba77b6d7f
LKWagnerUCF <82345540+LKWagner1967@users.noreply.github.com>
2021-09-08 20:22:21 -0500

    periodic save.

--- charts.js
+++ charts.js
@@ -63,39 +63,55 @@ function buildCharts(sample) {
     console.log("This is a test1.");
 
     // 3. Create a variable that holds the samples array. 
-    var metadata = data.metadata;
-    console.log(metadata);
+    var samples = data.samples;
+    console.log(samples);
     console.log("This is a test2.");
 
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    console.log(resultArray);
 
     //  5. Create a variable that holds the first sample in the array.
     var result = resultArray[0];
     console.log("This is a test3.");
-    console.log(result.otu_ids);
+    console.log(result);
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otu_ids = Object.entries(result.otu_ids);
+    console.log(otu_ids);
     var otu_labels = Object.entries(result.otu_labels);
+    console.log(otu_labels);
     var sample_values = Object.entries(result.sample_values);
+    console.log(sample_values);
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the top 10 otu_ids and map them in descending order
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = top_ten;
+    var sorted_ids = otu_ids.sort((a,b) => a-b);
+    var top_ten_ids = sorted_ids.slice(0,10);
 
-    var sorted_ids = otu_ids.sort((a,b) => a-b).reverse();
-    var top_ten = sorted_ids.slice(0,10);
+    var top_ten_samples = sample_values.slice(0,10);
+
+
+    var xticks = top_ten_samples.map(x=>x[1]);
+    console.log(xticks);
+
+    var yticks = top_ten_ids.map(y=>y[1]);
+    console.log(yticks);
+
+
 
 
     // 8. Create the trace for the bar chart. 
     var barData = {
-        x: sample_values,
+        x: xticks,
         y: yticks,
-        type: "bar"
+        type: "bar",
+        orientation: "h"
     };
+    var data = [barData];
+    console.log(data);
 
     // 9. Create the layout for the bar chart. 
     var barLayout = {
@@ -104,6 +120,6 @@ function buildCharts(sample) {
         yaxis: {title: "Bacteria Id"}
     };
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("bar", barData, barLayout)
+    Plotly.newPlot("bar", data, barLayout)
   });
 }
